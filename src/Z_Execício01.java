b16a2cedb23885d04b03c5488e249d3614131ff4
Guilherme Zatta <guilhermezatta@hotmail.com>
2024-04-19 21:45:13 -0300

    Exercício01 incompleta

--- src/Z_Execício01.java
+++ src/Z_Execício01.java
@@ -1,9 +1,12 @@
 public class Z_Execício01 {
-    public static void main(String[] args) throws Exception {
-        //Utilizando (While), construir um programa que apresente a soma dos cem primeiros números naturais (1 + 2 + 3 +...+ 98 + 99 + 100).
-        int numero = 1;
-        int soma = 0;
+    public static void main(String[] args) {
+        //Utilizando (While), construir um programa que apresente a soma 
+        //dos cem primeiros números naturais (1 + 2 + 3 +...+ 98 + 99 + 100).
+        int contador = 1;
 
-        while ()
+        while (contador <= 100) {
+            System.out.printf("%d \n", (contador));
+            contador = contador + 1;
+        }
     }
 }
