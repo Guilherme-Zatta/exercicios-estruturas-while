da88174ad0e0098c853f729ded9061bf97b14712
Guilherme Zatta <guilhermezatta@hotmail.com>
2024-04-19 21:28:33 -0300

    Primeiro Commit

---
+++ src/Z_Execício01.java
@@ -0,0 +1,9 @@
+public class Z_Execício01 {
+    public static void main(String[] args) throws Exception {
+        //Utilizando (While), construir um programa que apresente a soma dos cem primeiros números naturais (1 + 2 + 3 +...+ 98 + 99 + 100).
+        int numero = 1;
+        int soma = 0;
+
+        while ()
+    }
+}
